798e6aea5d0850da8759a22e452595d972c43fd9
ciaochiaociao <13917478+ciaochiaociao@users.noreply.github.com>
2024-04-27 13:19:34 -0500

    update info

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
-
+  rootNotionPageId: 'ecbb470232834a82837eaf3427b7bb23',
+  // https://www.notion.so/chiao-wei-hsu/My-Second-Brain-2edcdb621ee145beab70add958f9feef?pvs=4
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Chiao-Wei Hsu\'s Blog',
+  domain: 'chiao-wei-hsu-blog.vercel.app',
+  author: 'Chiao-Wei Hsu',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'A place to share my thoughts and ideas.',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'ciaochiaociao',
+  github: 'ciaochiaociao',
+  linkedin: 'chiao-wei-hsu',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
